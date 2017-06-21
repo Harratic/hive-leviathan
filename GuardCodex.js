149f5835d6b6b0b98f76bdb4bf5d992f785b874f
Thomas <Thomas@Thomas-Mac.local>
2017-06-21 13:54:31 -0600

    First commit

---
+++ GuardCodex.js
@@ -0,0 +1,14 @@
+
+const impGuard =
+	[
+		{ 	name: "Guardsmen",
+			toHit: "4",
+			strength: "3", //assume lasguns for now
+			toughness: "3",	
+		}	
+
+
+
+
+	]
+	
---
+++ OrkCodex.js
@@ -0,0 +1,35 @@
+
+const orks = 
+	[
+		{	Name: "Ork Boy",
+			toHit: "5",
+			strength: "4", //assuming shoota for now
+			toughness: "4"
+		}
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
